580f06ab00b23f7f8ef44f92031a957878245bc6
Alexander Vazhenin <a.vazhenin413@gmail.com>
2020-09-19 17:37:33 +0500

    Fixed human placement on grid

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 
     // Create Dino Constructor
-    function Animal(species, weight, height, diet, img) {
+    function Dino(species, weight, height, diet, where, when, fact, image) {
         this.species = species;
         this.weight = weight;
         this.height = height;
         this.diet = diet;
-        this.img = img;
-    };
-
-    function Dino(species, weight, height, diet, where, when, facts, img) {
-        Animal.call(this, species, weight, height, diet, img)
         this.where = where;
         this.when = when;
         this.fact = fact;
-    }
-    
-    Dino.prototype = Object.create(Animal.prototype)
+        this.image = image;
+    };
 
     // Create Dino Objects
     const triceratops = new Dino( 
@@ -25,8 +19,13 @@
             "herbavor",
             "North America",
             "Late Cretaceous",
-            "First discovered in 1889 by Othniel Charles Marsh",
-            "./images/triceratops.png"
+            [
+                "First discovered in 1889 by Othniel Charles Marsh",
+                "customfact2",
+                "customfact3",
+                "customfact4"
+            ],
+            "triceratops.png"
     );
     const tyrannosaurus = new Dino(
         "Tyrannosaurus Rex",
@@ -35,8 +34,13 @@
         "carnivor",
         "North America",
         "Late Cretaceous",
+        [
         "The largest known skull measures in at 5 feet long.",
-        "./images/tyrannosaurus rex.png"
+        "customfact2",
+        "customfact3",
+        "customfact4"
+        ],
+        "tyrannosaurus rex.png"
     );
 
     const anklyosaurus = new Dino(
@@ -46,8 +50,13 @@
         "herbavor",
         "North America",
         "Late Cretaceous",
+        [
         "Anklyosaurus survived for approximately 135 million years.",
-        "./images/anklyosaurus.png"
+        "customfact2",
+        "customfact3",
+        "customfact4"
+        ],
+        "anklyosaurus.png"
     );
 
     const brachiosaurus = new Dino(
@@ -57,8 +66,13 @@
         "herbavor",
         "North America",
         "Late Jurasic",
+        [
         "An asteroid was named 9954 Brachiosaurus in 1991.",
-        "./images/brachiosaurus.png"
+        "customfact2",
+        "customfact3",
+        "customfact4"
+        ],
+        "brachiosaurus.png"
     );
 
     const stegosaurus = new Dino(
@@ -68,8 +82,13 @@
         "herbavor",
         "North America, Europe, Asia",
         "Late Jurasic to Early Cretaceous",
+        [
         "The Stegosaurus had between 17 and 22 seperate places and flat spines.",
-        "./images/stegosaurus.png"
+        "customfact2",
+        "customfact3",
+        "customfact4"
+        ],
+        "stegosaurus.png"
     );
 
     const elasmosaurus  = new Dino(
@@ -79,8 +98,13 @@
         "carnivor",
         "North America",
         "Late Cretaceous",
+        [
         "Elasmosaurus was a marine reptile first discovered in Kansas.",
-        "./images/elasmosaurus.png"
+        "customfact2",
+        "customfact3",
+        "customfact4"
+        ],
+        "elasmosaurus.png"
     );
 
     const pteranodon = new Dino(
@@ -90,8 +114,13 @@
         "carnivor",
         "North America",
         "Late Cretaceous",
+        [
         "Actually a flying reptile, the Pteranodon is not a dinosaur.",
-        "./images/pteranodon.png"
+        "customfact2",
+        "customfact3",
+        "customfact4"
+        ],
+        "pteranodon.png"
     );
 
     const pigeon = new Dino(
@@ -101,103 +130,161 @@
         "herbavor",
         "World Wide",
         "Holocene",
+        [
         "All birds are living dinosaurs.",
-        "./images/pigeon.png"
+        "customfact2",
+        "customfact3",
+        "customfact4"
+        ],
+        "pigeon.png"
     );
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-    Dino.prototype.compareWeight = function (human) {
-        if (this.species !== human.species) {
-            if (this.weight > human.weight) {
-                this.facts.push(`${this.species} is heavier than ${human.species}`)
-            } else {
-                this.facts.push(`${human.species} is heavier than ${this.species}`)            
-            }
-        };
-    };
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-    Dino.prototype.compareHeight = function (human) {
-        if (this.species !== human.species) {
-            if (this.height > human.height) {
-                this.facts.push(`${this.species} is taller than ${human.species}`)
-            } else {
-                this.facts.push(`${this.species} is shorter than ${human.species}`)
-            }
-        };
-    };
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-    Dino.prototype.compareDiet = function (human) {
-        if (this.species !== human.species) {
-            this.facts.push(`${this.species} eats ${this.diet} while ${human.species} eats ${human.diet}`)
-        };
-    };
-
-    // Adding shuffle method
-    function shuffle(array) {
-
-        return array;
-    }
-
-    // Remove form from screen
-    // On button click, prepare and display infographic
-    const compareBtn = document.getElementById("btn");
-    const form = document.getElementById("dino-compare");
-    const grid = document.getElementById("grid");
-    grid.style.display = "none";
-
-    compareBtn.addEventListener("click", function () {
-        form.style.display = "none";
-        grid.style.display = "flex";
-
     // Use IIFE to get human data from form
-    let humanFromForm = (function getHumanData() {
-        const nameInput = document.getElementById("name").value;
-        const weightInput = document.getElementById("weight").value;
-        const feetHeightInput = document.getElementById("feet").value;
-        const inchesHeightInput = document.getElementById('inches').value;
-        const totalHeight = feetHeightInput * 12 + inchesHeightInput;
-        const dietInput = document.getElementById("diet");
-        
-        // Create Human Object
-        let human = new Dino(
-            nameInput,
-            weightInput,
-            totalHeight,
-            dietInput,
-            "human",
-            "Late Cretaceous",
-            "",
-            "./images/human.png"
-            );
-        return human;
+    function getHumanData() {
+        const human = (function () {
+            const name = document.getElementById("name").value;
+            const weight = document.getElementById("weight").value;
+            const feetHeight = document.getElementById("feet").value;
+            const inchesHeight = document.getElementById('inches').value;
+            const totalHeight = feetHeight * 12 + inchesHeight;
+            const diet = document.getElementById("diet").value;
+            const image = 'human.png';
+            
+            function nameInput() {
+                return name;
+            }
+
+            function weightInput() {
+                return weight; 
+            }
+
+            function totalHeightInput() {
+                return totalHeight;
+            }
+
+            function dietInput() {
+                return diet;
+            }
+
+            function getImage() {
+                return image;
+            }
+
+            return {
+                name: nameInput(),
+                weight: weightInput(),
+                height: totalHeightInput(),
+                diet: dietInput(),
+                image: getImage()
+            };
     })();
 
-    // Added all dinos, pigeon and human into array for future comparison
+    return human;
+}
+
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
+Dino.prototype.compareHeight = function (humanHeight) {
+    this.fact[1] = `The dino is ${
+      this.height / humanHeight
+    } times taller than you`;
+  };
+  
+  // Create Dino Compare Method 2
+  // NOTE: Weight in JSON file is in lbs, height in inches.
+  
+  Dino.prototype.compareWeight = function (humanWeight) {
+    this.fact[2] = `The dino is ${
+      this.weight / humanWeight
+    } times heavier than you`;
+  };
+  
+  // Create Dino Compare Method 3
+  // NOTE: Weight in JSON file is in lbs, height in inches.
+  
+  Dino.prototype.compareDiet = function (humanDiet) {
+    if (this.diet.toLowerCase() === humanDiet.toLowerCase()) {
+      this.fact[3] = `The dino had the same diet as yours which is ${this.diet}`;
+    } else {
+      this.fact[3] = `The dino had other diet which is ${this.diet}`;
+    }
+  };
+
+    // Generate Tiles for each Dino in Array
+
+    const human = getHumanData();
+
     let dinoArray = [
         triceratops,
         tyrannosaurus,
         anklyosaurus,
         brachiosaurus,
+        human,
         stegosaurus,
         elasmosaurus,
         pteranodon,
-        pigeon,
-        humanFromForm
-    ];
+        pigeon
+        ];
+
+    // Add tiles to DOM
+        function addTilesToDom(humanName) {
+            const grid = document.getElementById("grid");
+            dinoArray.map(dino => {
+                const tile = document.createElement('div');
+                tile.className = "grid-item";
+                
+                const title = document.createElement("h3");
+                title.className = "h3";
+                if (dino.species) {
+                    title.innerHTML = dino.species;
+                } else {
+                    title.innerHTML = humanName;
+                }
+
+                const fact = document.createElement("p");
+                fact.className = "p";
+                const factsArray = dino.fact;
+                let randomFact = "";
+
+                if (factsArray) {
+                    randomFact = factsArray[Math.floor(Math.random() * factsArray.length)];
+                }
+
+                if (dino.species == "Pigeon") {
+                    fact.innerHTML = dino.fact[0];
+                } else {
+                    fact.innerHTML = randomFact;
+                }
+
+                const image = document.createElement('img');
+                image.className = 'img';
+                image.src = `./images/${dino.image}`;
+            
+                tile.appendChild(image);
+                tile.appendChild(fact);
+                tile.appendChild(title);
+                grid.appendChild(tile);
+           });
+        }
+    // Remove form from screen
+    // On button click, prepare and display infographic
+
+    function removeFormFromScreen() {
+        const form = document.getElementById('dino-compare');
+        form.innerHTML = '';
+      }      
+        const compareBtn = document.getElementById('btn');
+        compareBtn.addEventListener('click', function () {
+        const human = getHumanData();
+        dinoArray.map(dino => {
+            if (dino.species) {
+                dino.compareHeight(human.height);
+                dino.compareWeight(human.weight);
+                dino.compareDiet(human.diet);
+    }
+  });
+  addTilesToDom(human.name);
+  removeFormFromScreen();
 });
-    
-
-    // Generate Tiles for each Dino in Array
-    const randomizeDinosInArray = shuffle(dinoArray);
-
-        // Add tiles to DOM
-
-
-
 
 
